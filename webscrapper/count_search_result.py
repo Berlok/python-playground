19579f59bdd30461002062eea25112808e93f994
Longfei Fu <10505374+berlok@users.noreply.github.com>
2017-08-31 16:10:33 +0200

    added scrapper for counting search results

---
+++ webscrapper/count_search_result.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+"""
+Python script for sending a HTTP POST request to a web search form,
+then scrape the result and print out total number of search results
+"""
+import urllib
+from bs4 import BeautifulSoup
+
+url = ""  # fill in the test url here
+post_params = {
+    'Birth date': '19000101',
+    'Family name': 'De Jong'
+}
+post_args = urllib.urlencode(post_params)
+
+fp = urllib.urlopen(url, post_args)
+soup = BeautifulSoup(fp, "html5lib")
+results = soup.find_all('h4', id="search_result")
+
+print "Total number of search result is " + str(len(results))
